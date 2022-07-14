274ff29fb61d43e01d11d5aeaea6aa7d4d62b9af
cpheighter <cheighterjr@gmail.com>
2022-07-14 11:11:21 -0400

    completed ToDo's

---
+++ CarLotSimulatorApp/CarLotSimulator/Methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    internal class Methods
+    {
+        public static void MakeEngineNoise(string x)
+        {
+            if (x == "Normal")
+            {
+                Console.WriteLine("The engine sounds good");
+            }
+            else
+            {
+                Console.WriteLine("Check Engine");
+            }
+        }
+        public static void MakeHonkNoise(string x)
+        {
+            if (x == "Loud")
+            {
+                Console.WriteLine("Honk noise level is good");
+            }
+            else
+            {
+                Console.WriteLine("Honk noise level isn't high enough");
+            }
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace CarLotSimulator
-{
+{ using CarLotSimulator;
     class Program
     {
         static void Main(string[] args)
@@ -15,9 +15,30 @@ namespace CarLotSimulator
 
 
             //Now that the Car class is created we can instanciate 3 new cars
+            Car car1 = new Car();
+            car1.Year = 2015;
+            car1.Make = "Lexus";
+            car1.Model = "RX";
+            car1.EngineNoise = "Normal";
+            car1.HonkNoise = "Loud";
+            car1.IsDriveable = true;
+
+            Car car2 = new Car(2008, "Toyota", "Rav4", "Normal", "Low", false);
+
+            var car3 = new Car() { Year = 1999, Make = "Honda", Model = "CR-V", EngineNoise = "Loud", HonkNoise = "Low", IsDriveable = false };
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            Methods.MakeEngineNoise(car1.EngineNoise);
+            Methods.MakeHonkNoise(car1.HonkNoise);
+            Console.WriteLine("-----------------------");
 
+            Methods.MakeEngineNoise(car2.EngineNoise);
+            Methods.MakeHonkNoise(car2.HonkNoise);
+            Console.WriteLine("-----------------------");
+
+            Methods.MakeEngineNoise(car3.EngineNoise);
+            Methods.MakeHonkNoise(car3.HonkNoise);
+            Console.WriteLine("-----------------------");
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
@@ -28,6 +49,15 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            var allCars = new CarLot();
+            allCars.Cars.Add(car1);
+            allCars.Cars.Add(car2);
+            allCars.Cars.Add(car3);
+
+            foreach (var car in allCars.Cars)
+            {
+                Console.WriteLine($"A {car.Year} {car.Make} {car.Model} engine noise is {car.EngineNoise} and the honk noise is {car.HonkNoise}. \nIs Driveable? {car.IsDriveable}\n");
+            }
         }
     }
 }
